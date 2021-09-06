4925b93a3926dc90894d94c03e1f54afa60e7989
jd-erreape <juandediosherrero@gmail.com>
2021-09-06 21:55:24 +0200

    [Issues/1] [Task] Create Home#index with empty view to hold Vue content

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe '#index' do
+    def do_index
+      get :index
+    end
+
+    before { do_index }
+
+    it 'returns a 200 status code' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns text/html content' do
+      expect(response.content_type).to eq('text/html; charset=utf-8')
+    end
+
+    it 'renders the template' do
+      expect(response).to render_template('index')
+    end
+  end
+end
