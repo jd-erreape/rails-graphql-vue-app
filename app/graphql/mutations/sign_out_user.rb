fee8b1443033214b0f0bac099e39a33a00533b85
jd-erreape <juandediosherrero@gmail.com>
2021-09-15 20:27:09 +0200

    [Issues/13] [Task] Log Out a User

---
+++ app/graphql/mutations/sign_out_user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Mutations
+  class SignOutUser < BaseMutation
+    null true
+
+    def resolve
+      # remove the token from session
+      context[:session] && context[:session][:token] = nil
+
+      # We are not returning anything as 
+      # part of this mutation, just deleting 
+      # the session token
+    end
+  end
+end
